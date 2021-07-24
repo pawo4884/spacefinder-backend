245455b6e7556e587b702e6295f3da82b0459934
Patrick Wolf <wolf.patrick@web.de>
2021-07-24 13:58:57 +0200

    modified to lambda list all s3 buckets incl. permissions policy

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,15 +1,11 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-
-import {
-  Code,
-  Function as LambdaFunction,
-  Runtime,
-} from "aws-cdk-lib/lib/aws-lambda";
 import { join } from "path";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/lib/aws-apigateway";
 import { GenericTable } from "./GenericTables";
 import { NodejsFunction } from "aws-cdk-lib/lib/aws-lambda-nodejs";
+import { PolicyStatement } from "aws-cdk-lib/lib/aws-iam";
+
 export class SpaceStack extends Stack {
   private api = new RestApi(this, "SpaceApi");
   private spacesTable = new GenericTable("SpacesTable", "spaceId", this);
@@ -17,18 +13,17 @@ export class SpaceStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    const helloLambda = new LambdaFunction(this, "helloLambda", {
-      runtime: Runtime.NODEJS_14_X,
-      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
-      handler: "hello.main",
-    });
-
     const helloLambdaNodeJs = new NodejsFunction(this, "helloLambdaNodeJs", {
       entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
       handler: "handler",
     });
 
-    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions("s3:ListAllMyBuckets");
+    s3ListPolicy.addResources("*");
+    helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
+
+    const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
     const helloLambdaRessource = this.api.root.addResource("hello");
     helloLambdaRessource.addMethod("GET", helloLambdaIntegration);
   }
