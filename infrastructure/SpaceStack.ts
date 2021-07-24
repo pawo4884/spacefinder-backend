09267be1c18cd7271c207f2c23180be97d9ef43c
Patrick Wolf <wolf.patrick@web.de>
2021-07-24 10:19:33 +0200

    with lambda get ressource

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,10 +1,27 @@
-import {Stack, StackProps} from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
 
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from "aws-cdk-lib/lib/aws-lambda";
+import { join } from "path";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/lib/aws-apigateway";
 export class SpaceStack extends Stack {
+  private api = new RestApi(this, "SpaceApi");
 
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
-  }
 
+    const helloLambda = new LambdaFunction(this, "helloLambda", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
+      handler: "hello.main",
+    });
+
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaRessource = this.api.root.addResource("hello");
+    helloLambdaRessource.addMethod("GET", helloLambdaIntegration);
+  }
 }
